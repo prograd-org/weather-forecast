6003b7a8796a6b187876a6353886724427a421cb
Sparsh Pathak <54447950+spathak98@users.noreply.github.com>
2022-03-21 14:50:45 +0530

    file updated

--- weather.js
+++ weather.js
@@ -1,67 +1,23 @@
-//Complete the Weather API Backend part using openweathermap api
- 
-let setAPI = async({ lat, long, api, part }) => {
-    try {
-        // let lat = 13.08784
-        // let lon = 80.27847
-        let data = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${part}&appid=${api}`)
-        if (data.status === 200)
-            return await data.json()
-    } catch (error) {
-        console.log(error);
 
-    }
+
+var apiKey = 'ca398e759895dcc24e3a1b9a43315752';
+
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var today = new Date();
+function getData()
+{
+
+    var input = document.getElementById('input').value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${apiKey}`
+    fetch(url)
+    .then(response=>response.json())
+    .then(data =>{
+        console.log(data)
+        document.querySelector('.city').innerHTML= data.name +" , "+ data.sys.country
+        document.querySelector('.date').innerHTML = weekDays[today.getDay()] + " " + today.getDate() + " " + month[today.getMonth()] + " " + today.getFullYear()
+        document.querySelector('.temp').innerHTML = data.main.temp +"°c"
+        document.querySelector('.weather').innerHTML=data.weather[0].main
+        document.querySelector('.hi-low').innerHTML = data.main.temp_min + "°c / "+data.main.temp_max +"°c"
+    })
 }
-
-const fetchdata = async() => {
-    try {
-        const showposiion = async(position) => {
-            let lat = position.coords.latitude
-            let long = position.coords.longitude
-                //let lat = 23.45
-                //let long = 94.04
-            const APIkey = 'fd612df03d9a6aa28a37f197dd6ee20e'
-            let data = await setAPI({
-                lat: lat,
-                long: long,
-                api: APIkey,
-                part: ''
-            })
-            console.log(data.current);
-
-            document.querySelector(".search-box").value = data.timezone
-            document.querySelector(".city").innerHTML = data.timezone
-            document.querySelector(".date").innerHTML = new Date(position.timestamp).toDateString()
-            document.querySelector(".temp").innerHTML = data.current.temp + "°c"
-
-            document.querySelector(".weather").innerHTML = (data.current.weather.map(ele => ele.main)).join(",")
-            document.querySelector(".hi-low").innerHTML = data.current.temp - 1 + "°c / " + (data.current.feels_like) + "°c"
-
-        }
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showposiion)
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-fetchdata()
-
-const mapapi = 'AIzaSyAq-Khiwsf4CsGDSC3JIbvo9vLYPdGaWy4'
-
-var getLocation = function(address) {
-
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'address': address }, function(results, status) {
-
-        if (status == google.maps.GeocoderStatus.OK) {
-            var latitude = results[0].geometry.location.lat();
-            var longitude = results[0].geometry.location.lng();
-            console.log(latitude, longitude);
-        }
-    });
-}
-
-getLocation('New York');
